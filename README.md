6f6be42c5989d162a2e5090a94b0483022c79956
Yash Parmar <yashp758820@gmail.com>
2024-08-29 20:57:59 +0530

    Updated README.md

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 // App logo will come here
 
 This [Flutter](https://flutter.dev/)-based mobile app brings AniList to your pocket. It enables anime and manga enthusiasts to browse a vast library of titles, effortlessly update their watching and reading lists, and connect with fellow fans.
+
+## Preview
+
+![App Screenshot](https://github.com/ternaryminds1970/otaku-world-rebase/blob/social-feature/github_assets/demo.gif?raw=true)
--- lib/core/ui/activities/text_activity_card.dart
+++ lib/core/ui/activities/text_activity_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:otaku_world/core/ui/activities/activity_base_card.dart';
+import 'package:otaku_world/core/ui/markdown/markdown.dart';
 import 'package:otaku_world/graphql/__generated/graphql/social/activities.graphql.dart';
 
 class TextActivityCard extends StatelessWidget {
@@ -18,9 +19,12 @@ class TextActivityCard extends StatelessWidget {
       likeCount: activity.likeCount,
       replyCount: activity.replyCount,
       timestamp: activity.createdAt,
-      child: Text(
-        activity.text!,
-        style: Theme.of(context).textTheme.headlineSmall,
+      // child: Text(
+      //   activity.text!,
+      //   style: Theme.of(context).textTheme.headlineSmall,
+      // ),
+      child: Markdown(
+        data: activity.text!,
       ),
     );
   }
