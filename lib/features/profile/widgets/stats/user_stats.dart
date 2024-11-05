ba89a6f0f62320b6e5b91b1a371c5917697feba7
Yash Parmar <yashp758820@gmail.com>
2024-11-05 16:18:55 +0530

    My Profile - User statistics bloc provider fix

--- lib/features/profile/widgets/stats/user_stats.dart
+++ lib/features/profile/widgets/stats/user_stats.dart
@@ -27,80 +27,82 @@ class _UserStatsState extends State<UserStats> {
   Widget build(BuildContext context) {
     final client = context.read<GraphqlClientCubit>().getClient()!;
 
-    // TODO: Provide bloc here
-    return BlocBuilder<UserStatsBloc, UserStatsState>(
-      builder: (context, state) {
-        if (state is UserStatsInitial) {
-          context.read<UserStatsBloc>().add(LoadUserStats(client));
-        }
-        if (state is UserStatsLoading || state is UserStatsInitial) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (state is UserStatsLoaded) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: CustomScrollView(
-              slivers: [
-                SliverPersistentHeader(
-                  pinned: true,
-                  delegate: _OptionDropdownDelegate(
-                    Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              isAnime ? 'Anime' : 'Manga',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headlineMedium
-                                  ?.copyWith(
-                                fontFamily: 'Poppins',
+    return BlocProvider(
+      create: (context) => UserStatsBloc(userId: widget.userId),
+      child: BlocBuilder<UserStatsBloc, UserStatsState>(
+        builder: (context, state) {
+          if (state is UserStatsInitial) {
+            context.read<UserStatsBloc>().add(LoadUserStats(client));
+          }
+          if (state is UserStatsLoading || state is UserStatsInitial) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is UserStatsLoaded) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: CustomScrollView(
+                slivers: [
+                  SliverPersistentHeader(
+                    pinned: true,
+                    delegate: _OptionDropdownDelegate(
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                isAnime ? 'Anime' : 'Manga',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headlineMedium
+                                    ?.copyWith(
+                                  fontFamily: 'Poppins',
+                                ),
                               ),
-                            ),
-                            Switch(
-                              value: isAnime,
-                              activeColor: AppColors.sunsetOrange,
-                              onChanged: (value) {
-                                setState(() {
-                                  isAnime = value;
-                                });
-                              },
-                            ),
-                          ],
-                        ),
-                        CustomDropdown(
-                          dropdownItems: StatsOption.values
-                              .map((e) => e.displayName)
-                              .toList(),
-                          initialValue: StatsOption.overview.displayName,
-                          onChange: (value) {
-                            setState(() {
-                              option = FilterConstants.statsOptionFromString(
-                                value,
-                              );
-                            });
-                          },
-                        ),
-                      ],
+                              Switch(
+                                value: isAnime,
+                                activeColor: AppColors.sunsetOrange,
+                                onChanged: (value) {
+                                  setState(() {
+                                    isAnime = value;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                          CustomDropdown(
+                            dropdownItems: StatsOption.values
+                                .map((e) => e.displayName)
+                                .toList(),
+                            initialValue: StatsOption.overview.displayName,
+                            onChange: (value) {
+                              setState(() {
+                                option = FilterConstants.statsOptionFromString(
+                                  value,
+                                );
+                              });
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                _buildStats(
-                  animeStats: state.animeStats,
-                  mangaStats: state.mangaStats,
-                ),
-              ],
-            ),
-          );
-        } else if (state is UserStatsError) {
-          return ErrorText(
-            message: state.message,
-            onTryAgain: () {},
-          );
-        } else {
-          return const Text('Unknown State');
-        }
-      },
+                  _buildStats(
+                    animeStats: state.animeStats,
+                    mangaStats: state.mangaStats,
+                  ),
+                ],
+              ),
+            );
+          } else if (state is UserStatsError) {
+            return ErrorText(
+              message: state.message,
+              onTryAgain: () {},
+            );
+          } else {
+            return const Text('Unknown State');
+          }
+        },
+      ),
     );
   }
 
