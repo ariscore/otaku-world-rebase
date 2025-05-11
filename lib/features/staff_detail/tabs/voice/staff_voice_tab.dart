8f2c2e6349b29feb7c3f9295abe41bc2395a8b83
Rakesh Chandrapal <rakesh202078@gmail.com>
2025-05-11 10:29:11 +0530

    Staff voice tab filter improved.

--- lib/features/staff_detail/tabs/voice/staff_voice_tab.dart
+++ lib/features/staff_detail/tabs/voice/staff_voice_tab.dart
@@ -20,74 +20,80 @@ class StaffVoiceTab extends StatefulWidget {
 class _StaffVoiceTabState extends State<StaffVoiceTab> {
   @override
   Widget build(BuildContext context) {
-      final bloc = context.read<StaffVoiceBloc>();
-    return BlocBuilder<StaffVoiceBloc, PaginatedDataState>(
-      builder: (context, state) {
-        if (state is PaginatedDataInitial || state is PaginatedDataLoading) {
-          return const SimpleLoading();
-        } else if (state is PaginatedDataError) {
-          return Center(child: Text('Error: ${state.message}'));
-        } else if (state is PaginatedDataLoaded) {
-          List<Query$staffVoice$Staff$characterMedia$edges?> staff =
-              state.list as List<Query$staffVoice$Staff$characterMedia$edges?>;
-          return NotificationListener<ScrollNotification>(
-            onNotification: (scrollInfo) {
-              if (scrollInfo.metrics.pixels ==
-                  scrollInfo.metrics.maxScrollExtent) {
-                final staffVoiceBloc = context.read<StaffVoiceBloc>();
-                final hasNextPage =
-                    (staffVoiceBloc.state as PaginatedDataLoaded).hasNextPage;
-                if (hasNextPage) {
-                  final client = (context.read<GraphqlClientCubit>().state
-                          as GraphqlClientInitialized)
-                      .client;
-                  staffVoiceBloc.add(LoadData(client));
-                }
-              }
-              return false;
+    final bloc = context.read<StaffVoiceBloc>();
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: MediaFilterWidget(
+            mediaSortNotifier: bloc.mediaSortNotifier,
+            isOnMyListNotifier: bloc.isOnMyList,
+            onApplyFilters: () {
+              final client = (context.read<GraphqlClientCubit>().state
+                      as GraphqlClientInitialized)
+                  .client;
+              bloc.applyFilter(client: client);
             },
-            child: CustomScrollView(
-              slivers: [
-                SliverToBoxAdapter(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: MediaFilterWidget(
-                      mediaSortNotifier: bloc.mediaSortNotifier,
-                      isOnMyListNotifier: bloc.isOnMyList,
-                      onApplyFilters: () {
+            bloc: bloc,
+          ),
+        ),
+        Expanded(
+          child: BlocBuilder<StaffVoiceBloc, PaginatedDataState>(
+            builder: (context, state) {
+              if (state is PaginatedDataInitial ||
+                  state is PaginatedDataLoading) {
+                return const SimpleLoading();
+              } else if (state is PaginatedDataError) {
+                return Center(child: Text('Error: ${state.message}'));
+              } else if (state is PaginatedDataLoaded) {
+                List<Query$staffVoice$Staff$characterMedia$edges?> staff = state
+                    .list as List<Query$staffVoice$Staff$characterMedia$edges?>;
+                return NotificationListener<ScrollNotification>(
+                  onNotification: (scrollInfo) {
+                    if (scrollInfo.metrics.pixels ==
+                        scrollInfo.metrics.maxScrollExtent) {
+                      final staffVoiceBloc = context.read<StaffVoiceBloc>();
+                      final hasNextPage =
+                          (staffVoiceBloc.state as PaginatedDataLoaded)
+                              .hasNextPage;
+                      if (hasNextPage) {
                         final client = (context.read<GraphqlClientCubit>().state
                                 as GraphqlClientInitialized)
                             .client;
-                        bloc.applyFilter(client: client);
-                      },
-                      bloc: bloc,
-                    ),
+                        staffVoiceBloc.add(LoadData(client));
+                      }
+                    }
+                    return false;
+                  },
+                  child: CustomScrollView(
+                    slivers: [
+                      SliverPadding(
+                        padding: const EdgeInsets.all(10),
+                        sliver: SliverList.separated(
+                          itemCount: staff.length,
+                          separatorBuilder: (context, index) =>
+                              const SizedBox(height: 10),
+                          itemBuilder: (context, index) {
+                            return StaffVoiceCard(
+                              staffEdge: staff[index],
+                            );
+                          },
+                        ),
+                      ),
+                      if (state.hasNextPage)
+                        const SliverToBoxAdapter(
+                          child: SimpleLoading(),
+                        ),
+                    ],
                   ),
-                ),
-                SliverPadding(
-                  padding: const EdgeInsets.all(10),
-                  sliver: SliverList.separated(
-                    itemCount: staff.length,
-                    separatorBuilder: (context, index) =>
-                        const SizedBox(height: 10),
-                    itemBuilder: (context, index) {
-                      return StaffVoiceCard(
-                        staffEdge: staff[index],
-                      );
-                    },
-                  ),
-                ),
-                if (state.hasNextPage)
-                  const SliverToBoxAdapter(
-                    child: SimpleLoading(),
-                  ),
-              ],
-            ),
-          );
-        } else {
-          return const Center(child: Text('No data available'));
-        }
-      },
+                );
+              } else {
+                return const Center(child: Text('No data available'));
+              }
+            },
+          ),
+        ),
+      ],
     );
   }
 }
